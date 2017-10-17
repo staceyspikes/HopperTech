3de90afc83c06c72b787d7bba9d58633b07da538
staceyspikesgit config --global user.name staceyspikes <stacey.lynne13@yahoo.com>
2017-10-16 20:45:58 -0400

    first commit

---
+++ Models/Course.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HopperTech.Models
+{
+    public class Course
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int CreditHours { get; set; }
+        public string Location { get; set; }
+        public string Department { get; set; }
+        public virtual ICollection<Student> Students { get; set; }
+        
+
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace HopperTech.Models
+{
+    public class Student
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public string HomeAddress { get; set; }
+        public string Email { get; set; }
+
+        [ForeignKey("Course")]
+        public int CourseID { get; set; }
+        public virtual Course Course { get; set; }
+    }
+}
